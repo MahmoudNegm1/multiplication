49ecfa236260ed00cdf605f266eecb980acef2c4
MahmoudNegm1 <77124033+MahmoudNegm1@users.noreply.github.com>
2022-11-25 10:13:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine(" please enter first number");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine( " please enter second number ");
+int SecondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine(" please enter your third number ");
+int ThirdNumber  = int.Parse(Console.ReadLine());
+Console.WriteLine($" Total = {firstNumber * SecondNumber * ThirdNumber}"); 
